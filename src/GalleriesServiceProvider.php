d96f4758a19d568c3a3e885e5e2052d9735397e6
egorr <yaroslavl.city@gmail.com>
2017-08-02 13:31:51 +0300

    Second commit

--- src/GalleriesServiceProvider.php
+++ src/GalleriesServiceProvider.php
@@ -9,14 +9,13 @@
 
 		public function boot()
 		{
-		$this->loadViewsFrom( __DIR__ . '/views', 'galleries' );
-		$this->loadRoutesFrom( __DIR__ . '/routes.php' );
-
-	/*		$this->publishes( [ __DIR__ . '/views' => resource_path( 'views/admin/galleries' ) ], 'galleries' );
-			$this->publishes( [ __DIR__ . '/config/galleries.php' => config_path( '/admin/galleries.php' ) ], 'galleriesConfig' );
+			$this->loadViewsFrom( __DIR__ . '/views', 'galleries' );
+			$this->loadRoutesFrom( __DIR__ . '/routes.php' );
+			$this->publishes( [ __DIR__ . '/views' => resource_path( 'views/admin/galleries' ) ], 'galleries' );
+			$this->publishes( [ __DIR__ . '/config/galleries.php' => config_path( 'admin/galleries.php' ) ], 'galleriesConfig' );
 			$this->publishes( [
 				__DIR__ . '/migrations/2017_07_19_060856_create_galleries_table.php' => base_path( 'database/migrations/2017_07_19_060856_create_galleries_table.php' )
-			], '' );*/
+			], '' );
 
 
 		}
@@ -24,7 +23,7 @@
 		public function register()
 		{
 
-	 		$this->app->make( 'Egorryaroslavl\Galleries\GalleriesController' );
+			$this->app->make( 'Egorryaroslavl\Galleries\GalleriesController' );
 			$this->app->make( 'Egorryaroslavl\Galleries\ImagesController' );
 			$this->mergeConfigFrom( __DIR__ . '/config/galleries.php', 'galleries' );
 		}
